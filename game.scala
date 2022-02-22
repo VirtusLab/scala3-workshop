792827ec71b69ad3c6a7548a97c2d0be16ad827a
Mikołaj Kozieł <mkoziel045@gmail.com>
2022-02-22 14:56:18 +0100

    Changed libraries

--- game.scala
+++ game.scala
@@ -1,252 +1,236 @@
-// using lib "com.propensive:gastronomy-core:0.3.0"
-// using lib "com.propensive:scintillate-server:0.3.0"
-// using lib "com.propensive:guillotine-core:0.3.0"
-// using lib "com.propensive:kaleidoscope-core:0.3.0"
-// using lib "com.propensive:euphemism-core:0.3.0"
-// using lib "org.typelevel:jawn-parser_3:1.2.0"
-// using lib "org.typelevel:jawn-ast_3:1.2.0"
-// using lib "com.propensive:acyclicity-core:0.3.0"
-// using lib "com.propensive:jovian-core:0.3.0"
-// using lib "com.propensive:honeycomb-core:0.3.0"
-// using lib "com.propensive:punctuation-html:0.3.0"
-// using lib "com.vladsch.flexmark:flexmark:0.42.12"
-// using lib "com.vladsch.flexmark:flexmark-ext-tables:0.42.12"
+//using lib "com.lihaoyi:cask_3:0.8.0"
+//using lib "com.lihaoyi:requests_3:0.7.0"
+//using lib "com.lihaoyi:scalatags_sjs1_3:0.11.0"
+//using lib "com.lihaoyi:os-lib_3:0.8.0"
+//using lib "com.lihaoyi:upickle_3:1.4.4"
+//using lib "com.github.vickumar1981:stringdistance_2.13:1.2.6"
 
-// using scala 3.1.0
+// using scala 3.0.0
 
 package workshop
 
-import gastronomy.*
-import gossamer.*
-import scintillate.*
-import rudiments.*
-import gesticulate.*
-import guillotine.*
-// import kaleidoscope.*
-import euphemism.*
-import escapade.*
-import jovian.*
-import eucalyptus.*
-import honeycomb.*
-import punctuation.*
+import cask.*
+import requests.{Response => rResponse, head=>rHead, *}
+import scalatags.Text.all.*
+import os.*
+import upickle.default.{ReadWriter => RW, macroRW, read, *}
+// import upickle.default.ReadWriter.join
+
+import com.github.vickumar1981.stringdistance.LevenshteinDistance
+
+import java.util.Base64
+import java.nio.charset.StandardCharsets
 
 import scala.concurrent.*
+import scala.util.Random
 
-given enc: Encoding = encodings.Utf8
-given Env = envs.enclosing
-given ExecutionContext = ExecutionContext.Implicits.global
-given LogFormat[Stdout.type, AnsiString] = LogFormat.timed
-given Log = Log(Everything |-> Stdout)
+object CaskHttpServer extends cask.MainRoutes{
 
-@main
-def game(): Unit = try
-  var continue = true
-  Log.info(t"Logging client into Spotify")
-  val auth = Spotify.clientCredentials()
-  Log.info(t"Getting playlist...")
-  val playlist: PlaylistRef = Spotify.playlists(auth, t"propensive").head
-  Log.info(t"Getting tracks from playlist...")
-  val items: List[Entry] = Spotify.playlistItems(auth, playlist)
-  Log.info(t"Fetched ${items.size} tracks")
-  Log.info(t"Starting web server on port 8080")
+    var continue = true
+    println("Logging client into Spotify")
+    val auth = Spotify.clientCredentials()
+    println("Getting playlist...")
+    val playlist: PlaylistRef = Spotify.playlists(auth, "propensive").head
+    println("Getting tracks from playlist...")
+    val items: List[Entry] = Spotify.playlistItems(auth, playlist)
+    println(s"Fetched ${items.size} tracks")
+    println("Starting web server on port 8080")
 
-  val secretKey = SymmetricKey.generate[Aes[256]]()
+    var authCode: Option[RequestAuth] = None
 
-  var authCode: Option[Auth] = None
-
-  // Define some request parameters
-  val CodeParam = RequestParam[Text](t"code")
-  val GuessParam = RequestParam[Text](t"guess")
-  val AnswerParam = RequestParam[Text](t"answer")
-
-  // Start web server
-  HttpServer(8080)
-    .listen {
-      request match
-        case UrlPath("/") =>
-          Response(
-            Template(
-              t"Start",
-              H1(t"Welcome"),
-              Div(md"Please [log in](/login) to play.".html)
-            )
-          )
-
-        case UrlPath("/start") & CodeParam(code) =>
-          val auth = Spotify.authorizationCode(code)
-          authCode = Some(auth)
-          Response(Redirect(t"/run"))
-
-        case UrlPath("/shutdown") =>
-          continue = false
-          Response(Redirect(t"/"))
-
-        case UrlPath("/run") =>
-          if authCode.isEmpty then Response(Redirect(t"/login"))
-          else
-            val chosen = items.random
-            val answer = chosen.track.name.text
-            val auth = authCode.get
-
-            try
-              Spotify.play(auth, playlist, items.indexOf(chosen))
-
-              Future {
-                Thread.sleep(7500)
-                Spotify.pause(auth)
-              }
-
-              Response(
-                Template(
-                  t"Guess!",
-                  H1(t"Guess the song!"),
-                  Div(t"Clue: ${chosen.track.name.initials}"),
-                  Form(
-                    method = Method.Post,
-                    autocomplete = false,
-                    action = t"/submit",
-                    enctype = media"application/x-www-form-urlencoded"
-                  )(
-                    Input(name = GuessParam),
-                    Input(
-                      name = AnswerParam,
-                      htype = HType.Hidden,
-                      value = secretKey.encrypt(answer).encode[Base64]
-                    ),
-                    Input(htype = HType.Submit, value = t"Guess")
-                  )
-                )
-              )
-
-            catch
-              case err: HttpError =>
-                err match
-                  case HttpError(HttpStatus.NotFound, content) =>
-                    if content
-                        .as[Json]
-                        .error
-                        .reason
-                        .as[Text] == t"NO_ACTIVE_DEVICE"
-                    then
-                      Response(
-                        Template(
-                          t"Error!",
-                          Div(t"Please open Spotify on your phone!")
-                        )
-                      )
-                    else
-                      Response(
-                        Template(
-                          t"Error!",
-                          Div(t"An unknown error has occurred")
-                        )
-                      )
-                  case _ =>
-                    Response(
-                      Template(t"Error!", Div(t"An unknown error has occurred"))
-                    )
-
-        case UrlPath("/login") =>
-          Response(Redirect(Spotify.loginUrl))
-
-        case UrlPath("/submit") & AnswerParam(answer) & GuessParam(guess) =>
-          val name = secretKey.decrypt[Text](answer.decode[Base64])
-          Response(
-            Template(
-              t"Your results...",
-              H1(
-                if name.lower.lev(guess.lower) < 4 then t"Well done!"
-                else t"Whoops..."
-              ),
-              Div(
-                md"The correct answer was _${name}_. [Another go](/run)?".html
-              )
-            )
-          )
-
-        case _ =>
-          Response(NotFound(Template(t"Not found", H1(t"Not found"))))
+    @get("/")
+    def hello() = {  
+      Template(
+          "Start",
+          h1("Welcome"),
+          div("Please", a(href:="/login")("log in"), " to play.")
+      )
+      // println(temp)
+      // cask.Response(
+      //   "Hello"
+      // )
     }
-    .await()
 
-catch
-  case err: Exception =>
-    println(err)
-    err.printStackTrace()
+    @get("/start")
+    def start(code: String) = {
+        val auth = Spotify.authorizationCode(code)
+        authCode = Some(auth)
+        Redirect("/run")
+    }
+
+    @get("/shutdown")
+    def shutdown() = {
+        continue = false
+        Redirect("/")
+    }
+
+    // @get("/run")
+    // def run() = {
+    //     if authCode.isEmpty then Redirect("/login")
+    //     else
+    //         val chosen = items(Random.nextInt(items.size))
+    //         val answer = chosen.track.name.text
+    //         val auth = authCode.get
+
+    //         try
+    //             Spotify.play(auth, playlist, items.indexOf(chosen))
+
+    //             Future {              // check Future
+    //                 Thread.sleep(7500)
+    //                 Spotify.pause(auth)
+    //             }
+
+    //             Response(
+    //                 Template(
+    //                     "Guess!",
+    //                     h1("Guess the song!"),
+    //                     div("Clue: ${chosen.track.name.initials}"),
+    //                     form(action := "/submit", method := "post")(
+    //                         input(
+    //                             `type` := "text", 
+    //                             name := "anwer", 
+    //                             placeholder := Base64.getEncoder.encodeToString(answer.getBytes(StandardCharsets.UTF_8)), 
+    //                             width := "0%"),
+    //                         input(
+    //                             `type` := "text", 
+    //                             name := "guess", 
+    //                             width := "80%"),
+    //                         input(
+    //                             `type` := "submit", 
+    //                             value := "Guess", 
+    //                             width := "20%")
+    //                     )
+    //                 )
+    //             )
+
+    //         catch
+    //             case err: Error =>
+    //                 Response(
+    //                     Template("Error!", div("An unknown error has occurred"))
+    //                 )
+    // }
+
+    // @get("/login")
+    // def login() = {
+    //     Redirect(Spotify.loginUrl)
+    // }
+
+    // @get("/submit")
+    // def submit(answer: String, guess: String) = {
+    //     val name = new String(Base64.getDecoder.decode(answer), StandardCharsets.UTF_8)
+    //     Response(
+    //         Template(
+    //             "Your results...",
+    //             h1(
+    //                 // if lev(name.toLowerCase(), guess.toLowerCase()) < 4 then "Well done!"
+    //                 if LevenshteinDistance.distance(name.toLowerCase(), guess.toLowerCase()) < 4 then "Well done!"
+    //                 else "Whoops..."
+    //             ),
+    //             div(
+    //                 s"The correct answer was ${name}. ", 
+    //                 a(href:="/run")("Another go?")
+    //             )
+    //         )
+    //     )
+    // }
+
+    initialize()
+}
 
 object Template:
-  def styles: Text = (Unix.Pwd / t"styles.css").file.read[Text](20.kb)
-  def apply(title: Text, content: Html[Flow]*): HtmlDoc =
-    HtmlDoc(Html(Head(Style(styles), Title(title)), Body(content*)))
+  def styles: String = os.read(os.pwd/"styles.css")
+  def apply(title: String, content: scalatags.generic.Modifier[scalatags.text.Builder]*)  =
+    // "<!DOCTYPE html>" + html(head(link(styles), tag("title")(title)), body(content: _*))
+  html(head(link(styles), tag("title")(title)), body(content: _*))
 
-given Json.Reader[Name] = summon[Json.Reader[Text]].map(Name(_))
-
-case class Name(text: Text):
-  def initials: Text = text.cut(t" ").map(_.take(1)).join(t"", t". ", t".")
+// case class Name(text: String) extends AnyVal
+type Name = String
+// object Name {
+//   def apply(name: String): Name = name
+// }
 
 case class Entry(track: Track)
 case class Track(album: Album, name: Name)
 case class Album(name: Name, artists: List[Artist])
 case class Artist(name: Name)
 case class Playlist(items: List[Entry])
-case class PlaylistRef(uri: Text, name: Name, id: Text)
+case class PlaylistRef(uri: String, name: Name, id: String)
 
 object Spotify:
-  private val ClientId = t"7154131346a640ee82dbd02ccfb72a50"
-  private val ClientSecret = t"f81e25c96ee44af7967179bb3c6998e2"
-  private val clientAuth = Auth.Basic(ClientId, ClientSecret)
-  private val RedirectUrl = t"http://introsgame.cc:8080/start"
-  private val urlEncoded = media"application/x-www-form-urlencoded"
+  private val ClientId = "7154131346a640ee82dbd02ccfb72a50"
+  private val ClientSecret = "f81e25c96ee44af7967179bb3c6998e2"
+  private val clientAuth = RequestAuth.Basic(ClientId, ClientSecret)
+  private val RedirectUrl = "http://introsgame.cc:8080/start"
+  private val urlEncoded = "application/x-www-form-urlencoded"
 
-  def clientCredentials(): Auth =
-    val url = uri"https://accounts.spotify.com/api/token".query(grant_type =
-      t"client_credentials"
-    )
-    val json = url
-      .post(clientAuth(), RequestHeader.ContentType(urlEncoded.show))(())
-      .as[Json]
-    Auth.Bearer(json.access_token.as[Text])
-
-  def authorizationCode(code: Text): Auth =
-    val url = uri"https://accounts.spotify.com/api/token".query(
-      grant_type = t"authorization_code",
-      code = code,
-      redirect_uri = RedirectUrl
+  def clientCredentials(): RequestAuth =
+    val r = requests.post(
+      "https://accounts.spotify.com/api/token", 
+      headers = Map("Content_Type" -> urlEncoded),
+      params = Map("grant_type" -> "client_credentials"),
+      auth = clientAuth
     )
 
-    val json = url
-      .post(clientAuth(), RequestHeader.ContentType(urlEncoded.show))(())
-      .as[Json]
-    Auth.Bearer(json.access_token.as[Text])
+    val rjson = ujson.read(r.text())
+    RequestAuth.Bearer(read(rjson("access_token")))
 
-  def loginUrl: Uri =
-    uri"https://accounts.spotify.com/authorize".query(
-      client_id = ClientId,
-      response_type = t"code",
-      redirect_uri = RedirectUrl,
-      scope = t"user-read-playback-state user-modify-playback-state"
+  def authorizationCode(code: String): RequestAuth = 
+    val r = requests.post(
+        "https://accounts.spotify.com/api/token",
+        headers = Map("Content_Type" -> urlEncoded),
+        params = Map(
+        "grant_type" -> "authorization_code",
+        "code" -> code,
+        "redirect_uri" -> RedirectUrl
+        ),
+        auth = clientAuth
+    )
+    RequestAuth.Bearer(r.headers("access_token").mkString(","))
+
+
+  def loginUrl: String =
+      "https://accounts.spotify.com/authorize" // TO-DO
+
+  def playlists(auth: RequestAuth, user: String): List[PlaylistRef] =
+    val r = requests.get(
+        s"https://api.spotify.com/v1/users/$user/playlists",
+        auth = auth
     )
 
-  def playlists(auth: Auth, user: Text): List[PlaylistRef] =
-    val json =
-      uri"https://api.spotify.com/v1/users/$user/playlists".get(auth()).as[Json]
-    json.items.as[List[PlaylistRef]]
+    val rjson = ujson.read(r.text())
+    given playlistRefRW:upickle.default.ReadWriter[PlaylistRef] = upickle.default.macroRW[PlaylistRef]
+    read[List[PlaylistRef]](rjson("items"))
 
-  def playlistItems(auth: Auth, playlist: PlaylistRef): List[Entry] =
-    val json = uri"https://api.spotify.com/v1/playlists/${playlist.id}/tracks"
-      .get(auth())
-      .as[Json]
-    json.items.as[List[Entry]]
+  def playlistItems(auth: RequestAuth, playlist: PlaylistRef): List[Entry] =
+    val r = requests.get(
+        s"https://api.spotify.com/v1/playlists/${playlist.id}/tracks",
+        auth = auth
+    )
 
-  def play(auth: Auth, playlist: PlaylistRef, offset: Int): HttpStatus =
-    case class Body(context_uri: Text, offset: Offset, position_ms: Int)
+    val rjson = ujson.read(r.text())
+
+    given artistR:upickle.default.Reader[Artist] = upickle.default.macroR[Artist]
+    given albumR:upickle.default.Reader[Album] = upickle.default.macroR[Album]
+    given trackR:upickle.default.Reader[Track] = upickle.default.macroR[Track]
+    given entryR:upickle.default.Reader[Entry] = upickle.default.macroR[Entry]
+    upickle.default.read[List[Entry]](rjson("items"))
+
+  def play(auth: RequestAuth, playlist: PlaylistRef, offset: Int)=//: HttpStatus =
     case class Offset(position: Int)
+    case class Body(context_uri: String, offset: Offset, position_ms: Int)
+    implicit def offsetR:upickle.default.ReadWriter[Offset] = upickle.default.macroRW[Offset]
+    implicit val rw: upickle.default.ReadWriter[Body] = upickle.default.macroRW[Body]
+
     val body = Body(playlist.uri, Offset(offset), 0)
-    uri"https://api.spotify.com/v1/me/player/play"
-      .put(auth())(body.json)
-      .as[HttpStatus]
+    
+    val r = requests.put(
+        "https://api.spotify.com/v1/me/player/play",
+        auth = auth,
+        // body = upickle.default.writable(body)
+    ) // TO-DO
 
-  def pause(auth: Auth): Unit =
-    uri"https://api.spotify.com/v1/me/player/pause".put(auth())(())
-
-given realm: Realm = Realm(t"game")
+  def pause(auth: RequestAuth): Unit = 
+      val r = requests.put(
+        "https://api.spotify.com/v1/me/player/pause",
+        auth = auth
+    )
